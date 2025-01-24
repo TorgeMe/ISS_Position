1c614119899fce9e9edfca3e5d1aed4db5844ca7
TorgeMe <tshopping@atkhauschild.de>
2025-01-24 09:18:58 +0100

    Add files via upload

--- ISS_4_0.py
+++ ISS_4_0.py
@@ -1,12 +1,6 @@
-### ISS ##################################
-# Version 4.0.3                          #
-#                                        #
-# vom 05.01.2025                         #
-#                                        #
-# @author: Torge Hauschild               #
-##########################################
-
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import Menu
 import requests
 import time
 
@@ -17,8 +11,41 @@ lat = 0
 lat_absolut = 0
 lat_direction = ""
 timestamp = 0
+status = "nicht aktiv"
 
-def iss_location():
+# Versions-Meldung
+def showVersion():
+    messagebox.showinfo("Version",
+                        "4.1.0 vom 20.01.2025")
+
+
+# Copyright-Meldung
+def showCopyright():
+    messagebox.showinfo("Copyright",
+                        "Torge Hauschild")
+
+# Hilfe-Meldung
+def showHelp():
+    messagebox.showinfo("Hilfe",
+    "Dieses Programm ermittelt die aktuelle Position der Internationalen Raumstation (ISS) \nin Echtzeit auf Grundlage der NASA-API \n  http://api.open-notify.org/iss-now.json")
+
+
+# Programm beenden
+def quitProgram():
+    decision = messagebox.askyesno("Beenden",
+                            "Möchten Sie das Programm beenden?")
+    if decision:
+        exit()
+
+
+# Fehler-Meldung V00
+def showErrorV00():
+    messagebox.showerror("Fehler V00",
+                        "Es ist ein Verbindungsfehler zur API aufgetreten!")
+
+
+# Daten von API ISS-now laden
+def loadData():
     response = requests.get("http://api.open-notify.org/iss-now.json")
     if response.status_code == 200:
         data = response.json()
@@ -27,19 +54,27 @@ def iss_location():
     else:
         return None
 
-def where():
-    iss_position = iss_location()
-    if iss_position:
-        lon = float(iss_position["longitude"])
-        lat = float(iss_position["latitude"])
-        timestamp = time.strftime("%d.%m.%Y %H:%M:%S")
-        return lon, lat, timestamp
-    else:
-        print("Fehler beim Abrufen der ISS-Position!")
 
-def refresh_gui():
-    global label_result1, label_result2, label_result3
-    lon, lat, timestamp = where()
+# Daten aus der json-response auslesen
+def getDataResponse():
+    try:
+        iss_position = loadData()
+        status = "Daten wurden verarbeitet"
+        if iss_position:
+            lon = float(iss_position["longitude"])
+            lat = float(iss_position["latitude"])
+            timestamp = time.strftime("%d.%m.%Y %H:%M:%S")
+            return lon, lat, timestamp, status
+    except:
+        status = "Verbindungsfehler"
+        timestamp = time.strftime("%d.%m.%Y %H:%M:%S")
+        showErrorV00()
+        return 0, 0, timestamp, status
+
+
+# Ergänze Richtungsdaten
+def addDataResponse():
+    lon, lat, timestamp, status = getDataResponse()
     
     if lon < 0:
         lon_direction = "West"
@@ -52,23 +87,59 @@ def refresh_gui():
     else:
         lat_direction = "Nord"
     lat_absolut = abs(lat)
+    return lon, lon_direction, lon_absolut, lat, lat_direction, lat_absolut, timestamp, status
+
+
+# Update GUI
+def refreshGui():
+    lon, lon_direction, lon_absolut, lat, lat_direction, lat_absolut, timestamp, status = addDataResponse()
     
-    # Update existing labels
+    label_status.config(text="Status: " + str(status))
     label_result1.config(text="Längengrad = " + str(lon_absolut) + " " + str(lon_direction))
     label_result2.config(text="Breitengrad = " + str(lat_absolut) + " " + str(lat_direction))
     label_result3.config(text="Zeitpunkt = " + str(timestamp))
 
+
+# Erstelle GUI
 def gui():
-    global label_result1, label_result2, label_result3
+    # globale Variablen definieren
+    global label_result1, label_result2, label_result3, label_status
+
+    # Fenster
     main = tk.Tk()
     main.title("ISS - Internationale Raumstation")
-    
+
+    # Menüleiste
+    menubar = Menu(main)
+    main.config(menu=menubar)
+    info_menu = Menu(menubar, tearoff=0)
+    exit_menu = Menu(menubar, tearoff=0)
+
+    info_menu.add_command(label="Version", command=showVersion)
+    info_menu.add_separator() # Fügt eine Trennlinie hinzu
+    info_menu.add_command(label="Copyright", command=showCopyright)
+    info_menu.add_separator() # Fügt eine Trennlinie hinzu
+    info_menu.add_command(label="Hilfe", command=showHelp)
+
+    exit_menu.add_command(label="Beenden", command=quitProgram)
+
+    menubar.add_cascade(label="Info", menu=info_menu)
+    menubar.add_cascade(label="Beenden", menu=exit_menu)
+
+    main.config(menu=menubar)  
+
+    # Fenster-Abmessungen
     main.geometry("800x400")
     main.minsize(400,200)
+
+    # Fenster-Inhalt
     label_headline = tk.Label(main, 
-                     text="Aktuelle Position der ISS",
-                     font=("Arial,20"))
-    
+                            text="Aktuelle Position der ISS",
+                            font=("Arial,20"))
+    label_status = tk.Label(main,
+                            text="Status: " + str(status),
+                            font=("Arial,12"))
+
     label_result1 = tk.Label(main,
                             text="Längengrad = " + str(lon_absolut) + " " + str(lon_direction),
                             font=("Arial,12"))
@@ -81,17 +152,21 @@ def gui():
                             text="Zeitpunkt = " + str(timestamp),
                             font=("Arial,12"))
     
-    button_Ermittle = tk.Button(main,
-                               text=("Positionsermittlung"),
-                               command=refresh_gui)
+    button_ermittle = tk.Button(main,
+                               text=("POSITIONSERMITTLUNG"),
+                               command=refreshGui)
+
     
-    label_headline.pack(pady=50)
+    # Fenster erstellen
+    label_headline.pack(pady=25)
+    label_status.pack()
     label_result1.pack()
     label_result2.pack()
     label_result3.pack()
-    button_Ermittle.pack(pady=50)
+    button_ermittle.pack(pady=25)
     
     main.mainloop()
 
+# Hauptprogramm
 if __name__ == "__main__":
     gui()
