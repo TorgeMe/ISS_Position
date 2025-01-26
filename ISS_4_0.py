5f7db3904b9d67f604f6324f277c39f5aea07114
TorgeMe <tshopping@atkhauschild.de>
2025-01-26 19:33:04 +0100

    komplett überarbeitet
    alles umgebaut

--- ISS_4_0.py
+++ ISS_4_0.py
@@ -3,6 +3,9 @@ from tkinter import messagebox
 from tkinter import Menu
 import requests
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.basemap import Basemap
 
 lon = 0
 lon_absolut = 0
@@ -16,7 +19,11 @@ status = "nicht aktiv"
 # Versions-Meldung
 def showVersion():
     messagebox.showinfo("Version",
+<<<<<<< Updated upstream
                         "4.1.0 vom 20.01.2025")
+=======
+                        "4.2.2 vom 26.01.2025")
+>>>>>>> Stashed changes
 
 
 # Copyright-Meldung
@@ -43,6 +50,14 @@ def showErrorV00():
     messagebox.showerror("Fehler V00",
                         "Es ist ein Verbindungsfehler zur API aufgetreten!")
 
+<<<<<<< Updated upstream
+=======
+# Einstellungsmenü
+def Einstellungen():
+    messagebox.showinfo("Einstellungen",
+                        "API-URL: http://api.open-notify.org/iss-now.json")                        
+
+>>>>>>> Stashed changes
 
 # Daten von API ISS-now laden
 def loadData():
@@ -87,6 +102,7 @@ def addDataResponse():
     else:
         lat_direction = "Nord"
     lat_absolut = abs(lat)
+<<<<<<< Updated upstream
     return lon, lon_direction, lon_absolut, lat, lat_direction, lat_absolut, timestamp, status
 
 
@@ -104,6 +120,55 @@ def refreshGui():
 def gui():
     # globale Variablen definieren
     global label_result1, label_result2, label_result3, label_status
+=======
+    return lon_direction, lon_absolut, lat_direction, lat_absolut, timestamp, status
+
+
+# Update GUI
+def refreshGui():
+    lon_direction, lon_absolut, lat_direction, lat_absolut, timestamp, status = addDataResponse()
+    
+    label_status_0.config(text="Status: ")
+    label_status_1.config(text=str(status))
+    label_result1_0.config(text="Längengrad = ")
+    label_result1_1.config(text=str(lon_absolut) + " " + str(lon_direction))
+    label_result2_0.config(text="Breitengrad = ")
+    label_result2_1.config(text=str(lat_absolut) + " " + str(lat_direction))
+    label_result3_0.config(text="Zeitpunkt = ")
+    label_result3_1.config(text=str(timestamp))
+
+
+# Zeichne Karte
+def map():
+    refreshGui()
+    iss_position = loadData()
+    if iss_position: 
+        try:
+            plt.clf()
+        finally:
+            lon = float(iss_position['longitude'])
+            lat = float(iss_position['latitude'])
+        
+            karte = Basemap(projection='ortho', lat_0=lat, lon_0=lon)
+            karte.drawcoastlines()
+            karte.drawmapboundary(fill_color='aqua')
+            karte.fillcontinents(color='coral',lake_color='aqua')
+            karte.drawparallels(np.arange(-90.,120.,30.))
+            karte.drawmeridians(np.arange(0.,360.,60.))
+        
+            x, y = karte(lon, lat)
+            karte.plot(x, y, 'ro', markersize=10)
+            plt.title("Aktuelle Position der ISS")
+            plt.show()
+
+# Erstelle GUI
+def gui():
+    # globale Variablen definieren
+    global label_result1_0, label_result1_1
+    global label_result2_0, label_result2_1 
+    global label_result3_0, label_result3_1
+    global label_status_0, label_status_1
+>>>>>>> Stashed changes
 
     # Fenster
     main = tk.Tk()
@@ -119,6 +184,11 @@ def gui():
     info_menu.add_separator() # Fügt eine Trennlinie hinzu
     info_menu.add_command(label="Copyright", command=showCopyright)
     info_menu.add_separator() # Fügt eine Trennlinie hinzu
+<<<<<<< Updated upstream
+=======
+    info_menu.add_command(label="Einstellungen", command=Einstellungen)
+    info_menu.add_separator() # Fügt eine Trennlinie hinzu
+>>>>>>> Stashed changes
     info_menu.add_command(label="Hilfe", command=showHelp)
 
     exit_menu.add_command(label="Beenden", command=quitProgram)
@@ -136,22 +206,32 @@ def gui():
     label_headline = tk.Label(main, 
                             text="Aktuelle Position der ISS",
                             font=("Arial,20"))
+<<<<<<< Updated upstream
     label_status = tk.Label(main,
                             text="Status: " + str(status),
                             font=("Arial,12"))
 
     label_result1 = tk.Label(main,
                             text="Längengrad = " + str(lon_absolut) + " " + str(lon_direction),
+=======
+    label_status_0 = tk.Label(main,
+                            text="Status: ",
+>>>>>>> Stashed changes
                             font=("Arial,12"))
     
-    label_result2 = tk.Label(main,
-                            text="Breitengrad = " + str(lat_absolut) + " " + str(lat_direction),
+    label_status_1 = tk.Label(main,
+                            text=str(status),
+                            font=("Arial,12"))
+
+    label_result1_0 = tk.Label(main,
+                            text="Längengrad = ",
                             font=("Arial,12"))
     
-    label_result3 = tk.Label(main,
-                            text="Zeitpunkt = " + str(timestamp),
+    label_result1_1 = tk.Label(main,
+                            text=str(lon_absolut) + " " + str(lon_direction),
                             font=("Arial,12"))
     
+<<<<<<< Updated upstream
     button_ermittle = tk.Button(main,
                                text=("POSITIONSERMITTLUNG"),
                                command=refreshGui)
@@ -164,6 +244,47 @@ def gui():
     label_result2.pack()
     label_result3.pack()
     button_ermittle.pack(pady=25)
+=======
+    label_result2_0 = tk.Label(main,
+                            text="Breitengrad = ",
+                            font=("Arial,12"))
+    
+    label_result2_1 = tk.Label(main,
+                            text=str(lat_absolut) + " " + str(lat_direction),
+                            font=("Arial,12"))
+    
+    label_result3_0 = tk.Label(main,
+                            text="Zeitpunkt = ",
+                            font=("Arial,12"))
+    
+    label_result3_1 = tk.Label(main,
+                            text=str(timestamp),
+                            font=("Arial,12"))
+    
+    button_ermittle = tk.Button(main,
+                               text=("D A T E N"),
+                               command=refreshGui)
+    
+    button_map = tk.Button(main,
+                           text=("K A R T E"),
+                           command=map)
+
+    
+    # Fenster erstellen
+    main.columnconfigure(0, weight=1)
+    main.columnconfigure(1, weight=5)
+    label_headline.grid(row=0, sticky=tk.N, pady=50, padx=150)
+    label_status_0.grid(row=2, column=0, sticky=tk.W, padx=100)
+    label_status_1.grid(row=2, column=1, sticky=tk.W)
+    label_result1_0.grid(row=3, column=0, sticky=tk.W, padx=100)
+    label_result1_1.grid(row=3, column=1, sticky=tk.W)
+    label_result2_0.grid(row=4, column=0, sticky=tk.W, padx=100)
+    label_result2_1.grid(row=4, column=1, sticky=tk.W)
+    label_result3_0.grid(row=5, column=0, sticky=tk.W, padx=100)
+    label_result3_1.grid(row=5, column=1, sticky=tk.W)
+    button_ermittle.grid(row=7, column=0, pady=40)
+    button_map.grid(row=7, column=0, sticky=tk.E)
+>>>>>>> Stashed changes
     
     main.mainloop()
 
